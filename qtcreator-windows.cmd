919522674d560c050234e096107b028c4eddf702
Aaron Ten Clay <aarontc@aarontc.com>
2013-02-17 18:11:06 -0800

    Added copyright and license headers

--- qtcreator-windows.cmd
+++
@@ -1,3 +0,0 @@
-set QTVERSION=4.8.1
-set PATH=C:\QtSDK\mingw\bin;C:\QtSDK\Desktop\Qt\%QTVERSION%\mingw\lib;C:\QtSDK\Desktop\Qt\%QTVERSION%\mingw\bin;C:\Windows\system32;C:\Program Files (x86)\CMake 2.8\bin
-C:\QtSDK\QtCreator\bin\qtcreator.exe CMakeLists.txt
